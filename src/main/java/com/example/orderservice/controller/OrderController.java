711ac7ae4500fd88eab649f1afdaa449331f8977
GyuHyeong <cjsworbehd13@gmail.com>
2021-10-04 17:02:02 +0900

    장애처리용 Resilience4j 이용 circuitbreaker적용, 분산추적용 Zipkin 적용 sleuth + zipkin 이용한 분산추적 적용 micrometer-prometheus, grafana 적용한 모니터링 기능 적용

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -38,35 +38,49 @@ public class OrderController {
 
     @PostMapping("/{userId}/orders")
     public ResponseEntity<ResponseOrder> createUser(@PathVariable String userId, @Valid @RequestBody RequestOrder requestOrder){
+        log.info("Before add orders data");
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         OrderDto orderDto = modelMapper.map(requestOrder, OrderDto.class);
         orderDto.setUserId(userId);
 
         /* jpa */
-//        OrderDto dto = orderService.createOrder(reqDto);
-//        ResponseOrder responseOrder = modelMapper.map(dto, ResponseOrder.class);
+        OrderDto dto = orderService.createOrder(orderDto);
+        ResponseOrder responseOrder = modelMapper.map(dto, ResponseOrder.class);
 
         /* Kafka */
-        orderDto.setOrderId(UUID.randomUUID().toString());
-        orderDto.setTotalPrice(requestOrder.getQty() * requestOrder.getUnitPrice());
-        ResponseOrder responseOrder = modelMapper.map(orderDto, ResponseOrder.class);
+//        orderDto.setOrderId(UUID.randomUUID().toString());
+//        orderDto.setTotalPrice(requestOrder.getQty() * requestOrder.getUnitPrice());
+
 
         /* send this order to the kafka */
-        kafkaProducer.send("example-catalog-topic", orderDto);
-        orderProducer.send("orders", orderDto);
+//        kafkaProducer.send("example-catalog-topic", orderDto);
+//        orderProducer.send("orders", orderDto);
 
+//        ResponseOrder responseOrder = modelMapper.map(orderDto, ResponseOrder.class);
+
+        log.info("After added orders data");
         return new ResponseEntity<>(responseOrder, HttpStatus.CREATED);
     }
 //
     @GetMapping("/{userId}/orders")
-    public ResponseEntity<List<ResponseOrder>> createUser(@PathVariable String userId){
+    public ResponseEntity<List<ResponseOrder>> createUser(@PathVariable String userId) throws Exception {
+        log.info("Before retrieve orders data");
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         Iterable<OrderEntity> orderEntities = orderService.getOrdersByUserId(userId);
         List<ResponseOrder> responseOrders = new ArrayList<>();
         orderEntities.forEach(entity -> responseOrders.add(modelMapper.map(entity, ResponseOrder.class)));
+
+        try{
+            Thread.sleep(1000);
+            throw new Exception("장애 발생");
+        }catch (InterruptedException e){
+            log.warn(e.getMessage());
+        }
+
+        log.info("After retrieved orders data");
 
         return new ResponseEntity<>(responseOrders, HttpStatus.CREATED);
     }
